4773626ed5e62b894b1e12000ccfaee505256a71
kumar_shashank <shashank.kumar@azent.com>
2023-09-26 17:16:51 +0530

    random ClassNames being added to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/marketingApp';
 import Header from './components/Header';
-
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+const containerGenerateClassName = createGenerateClassName({
+  productionPrefix: 'container'
+})
 export default () => {
 
   return (
     <>
       <BrowserRouter>
-        <div>
-          <Header />
-          <MarketingApp />
-        </div>
+        <StylesProvider generateClassName={containerGenerateClassName}>
+          <div>
+            <Header />
+            <MarketingApp />
+          </div>
+        </StylesProvider>
       </BrowserRouter>
     </>
 
