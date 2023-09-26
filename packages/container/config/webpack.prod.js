6b04854c1b0213925d7959e26b3c9099b127ad47
kumar_shashank <shashank.kumar@azent.com>
2023-09-26 16:23:05 +0530

    Created Container Deploy script

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,2 +1,26 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { merge } = require('webpack-merge');
+const commonConfig = require("./webpack.common")
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
+const domain = process.env.PRODUCTION_DOMAIN
 
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js'`
+      },
+      shared: packageJson.dependencies
+    })
+  ]
+  // plugins:[{
 
+  // }]
+}
+module.exports = merge(prodConfig, commonConfig)
